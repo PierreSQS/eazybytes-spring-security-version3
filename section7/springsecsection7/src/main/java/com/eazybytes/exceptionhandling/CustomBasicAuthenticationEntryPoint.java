040cebde6193b1c986f22cb89f337754b52796a1
PierreSQS <nomo_messina@web.de>
2025-04-23 15:32:18 +0200

    Sec7_Chap53: step2 - customized response body

--- section7/springsecsection7/src/main/java/com/eazybytes/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
+++ section7/springsecsection7/src/main/java/com/eazybytes/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
@@ -8,15 +8,31 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
 
+        // Populate dynamic values
+        String message =
+                (authException != null && authException.getMessage() != null) ?
+                        authException.getMessage(): "Unauthenticated";
+
+        response.setLocale(Locale.ENGLISH);
         response.setHeader("eazybank-error-reason", "Authentication Failed!");
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
+        String path = request.getServletPath();
 
+        // Construct the JSON response
+        String jsonResp =
+                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
+                        LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                        message, path);
+
+        response.getWriter().append(jsonResp);
     }
 }

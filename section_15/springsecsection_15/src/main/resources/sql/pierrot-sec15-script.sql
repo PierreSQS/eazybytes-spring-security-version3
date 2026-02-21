419fd9ef74e3c4d78ee7d076e094e7213a248b45
PierreSQS <nomo_messina@web.de>
2026-02-21 13:41:17 +0100

    Sec15_Chap142-XX: fixed the custom Pierrot DB

--- section_15/springsecsection_15/src/main/resources/sql/pierrot-sec15-script.sql
+++ section_15/springsecsection_15/src/main/resources/sql/pierrot-sec15-script.sql
@@ -3,15 +3,16 @@ create database eazybanksection15_db;
 
 use eazybanksection15_db;
 
-drop table if exists `authorities`;
-drop table if exists `users`;
-drop table if exists `customer`;
-drop table if exists `accounts`;
-drop table if exists `cards`;
-drop table if exists `loans`;
-drop table if exists `account_transactions`;
-drop table if exists `contact_messages`;
-drop table if exists `notice_details`;
+-- NO NEEDED ANYMORE SINCE WE DELETE THE DB OUTWRIGHT
+-- drop table if exists `authorities`;
+-- drop table if exists `users`;
+-- drop table if exists `customer`;
+-- drop table if exists `accounts`;
+-- drop table if exists `cards`;
+-- drop table if exists `loans`;
+-- drop table if exists `account_transactions`;
+-- drop table if exists `contact_messages`;
+-- drop table if exists `notice_details`;
 
 CREATE TABLE `customer` (
                             `customer_id` int NOT NULL AUTO_INCREMENT,

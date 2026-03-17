a9e575520b2dc9c7049ca1a432fc095773a4aa19
PierreSQS <nomo_messina@web.de>
2026-03-17 13:21:56 +0100

    Sec4_Chap33-XX: minor code optimizations

--- section4/springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section4/springsecsection4/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,20 +16,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((requests) -> requests
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
-
-    /*@Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,7 +33,7 @@ public class ProjectSecurityConfig {
 
     /**
      * From Spring Security 6.3 version
-     * @return
+     * @return CompromisedPasswordChecker
      */
     @Bean
     public CompromisedPasswordChecker compromisedPasswordChecker() {
